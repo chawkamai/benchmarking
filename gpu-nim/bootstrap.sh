81b2173945f6ed2aed53f40bc92986e3e1057f06
Chester Hawkins <chawkins@akamai.com>
2025-11-12 11:08:25 -0800

    updates

--- gpu-nim/bootstrap.sh
+++ gpu-nim/bootstrap.sh
@@ -2,7 +2,7 @@
 
 VENV_DIR=python-venv
 
-sudo apt install python3-venv python-is-python3 make gcc nvtop
+sudo apt install -y python3-venv python-is-python3 make gcc nvtop
 
 if [ ! -d $VENV_DIR ]; then
     echo "Creating '$VEND_DIR' Python virtual environment..."
